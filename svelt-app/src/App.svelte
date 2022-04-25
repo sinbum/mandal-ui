d4696e058297d2abec49767440e63871aa762b3b
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-04-25 23:39:52 +0900

    일부수정

--- svelt-app/src/App.svelte
+++ svelt-app/src/App.svelte
@@ -1,49 +1,58 @@
 <script>
-	import { Router, Link, Route } from "svelte-navigator";
-	import Header from "./pages/header/header.svelte";
-	import Nav from "./pages/nav/nav.svelte";
-	import Mandal from "./pages/main/section/mandal.svelte";
-	import Login from "./pages/login/login.svelte";
+import {
+    Router,
+    Link,
+    Route
+} from "svelte-navigator";
+import Header from "./pages/header/header.svelte";
+import Nav from "./pages/nav/nav.svelte";
+import Mandal from "./pages/main/section/mandal.svelte";
+import Login from "./pages/login/login.svelte";
 
-	
-	import { storeName } from './store.js'	
-	
-	$storeName = 'WORLD!!';
+import {
+    storeName
+} from './store.js'
 
-	
+$storeName = 'WORLD!!';
 </script>
 
 <Router>
-	
-		<header>
-			<Header />
-		</header>
 
-	<nav>		
-		<Nav />
-	</nav>
+    <header>
+        <Header />
+    </header>
 
-	<main>
+    <div class="container">
+        <nav>
+            <Nav />
+        </nav>
 
-		<Route path="/">
-			<Mandal />
-		</Route>
+        <main>
 
-		<Route path="login">
-			<Login />
-		</Route>
+            <Route path="/">
+                <Mandal />
+            </Route>
 
-		<!-- <Route path="about">
-		<h3>About</h3>
-		<p>That's what it's all about!</p>
-	  </Route> -->
+            <Route path="login">
+                <Login />
+            </Route>
 
-		<!-- <PrivateRoute path="profile" let:location>
-		<h3>Welcome {$user.username}</h3>
-		<button on:click={handleLogout}>Logout</button>
-	  </PrivateRoute> -->
-	</main>
+            <!-- <Route path="about">
 
+<h3>About</h3>
+
+<p>That's what it's all about!</p>
+            </Route> -->
+
+            <!-- <PrivateRoute path="profile" let:location>
+
+<h3>Welcome {$user.username}</h3>
+
+<button on:click={handleLogout}>Logout</button>
+            </PrivateRoute> -->
+        </main>
+
+    </div>
 
 </Router>
 
