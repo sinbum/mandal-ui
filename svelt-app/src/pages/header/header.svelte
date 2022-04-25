534d6c1d8126836fa92257784bf67939c795b832
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-04-25 17:44:11 +0900

    Update header.svelte

--- svelt-app/src/pages/header/header.svelte
+++ svelt-app/src/pages/header/header.svelte
@@ -1,1 +1,16 @@
-<h1 style="text-align: center;">History</h1>
+<div class="header">
+    <h1 class="title">History</h1>
+</div>
+
+<style>
+    .header {
+      background-color: darkslategrey;
+      padding: 8px;
+      width: 100%;
+    }
+  
+    .header > .title {
+      text-align: center;
+      color: white;
+    }
+  </style>
